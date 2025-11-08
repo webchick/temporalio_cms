7873aca3263ec20296948f9d0c1d52c724297a9c
Angie Byron <webchick@webchick.net>
2025-11-08 13:44:07 -0800

    Oops, missed a spot.

--- docker/stack.sh
+++ docker/stack.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+# Ensure the Temporal submodule is initialized before running compose.
+git submodule update --init --recursive >/dev/null 2>&1
+
 COMPOSE_FILES=("-f" "temporal/docker-compose.yml" "-f" "docker-compose.overlay.yml")
 exec docker compose "${COMPOSE_FILES[@]}" "$@"
